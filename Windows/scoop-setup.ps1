04fb62d822c0b209a06969fe43f923d779e88beb
Andrew Cotter <cotter.andrew.p@gmail.com>
2024-03-05 09:49:59 -0500

    Create scoop-setup.ps1

---
+++ Windows/scoop-setup.ps1
@@ -0,0 +1,45 @@
+# Install Scoop
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+# Install scoop buckets
+scoop bucket add extras
+scoop bucket add games
+scoop bucket add nerd-fonts
+
+# Install desired programs using Scoop
+$scoopApps = @(
+    "7zip",
+    "AnonymousPro-NF",
+    "arduino",
+    "blender",
+    "bottom",
+    "cmake",
+    "curl",
+    "deno",
+    "docker",
+    "ffmpeg",
+    "git",
+    "github",
+    "godot",
+    "helix",
+    "jetbrains-toolbox"
+    "lazydocker",
+    "ntop",
+    "nvm",
+    "obsidian",
+    "oh-my-posh",
+    "openscad",
+    "pandoc",
+    "postman",
+    "rustup",
+    "slack",
+    "vlc",
+    "winfetch",
+    "wiztree",
+    "yt-dlp"
+)
+
+foreach ($app in $scoopApps) {
+    scoop install $app
+}
